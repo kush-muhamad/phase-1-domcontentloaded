372ccc089e1fe25ca1973654dce444554d95d216
kush Muhamad <kushmohammed98@yahoo.com>
2023-03-21 19:47:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",function() {
+    const textInfo = document.getElementById('text')
+    textInfo.textContent = "This is really cool!"
+    console.log(textInfo)
+    
+  })
